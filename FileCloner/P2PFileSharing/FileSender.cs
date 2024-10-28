f16aba75180247f28e1e3452010c7fba725fa6a5
NEERAJ KRISHNA N <neerajnjps03@gmail.com>
2024-10-28 16:58:02 +0530

    added FileCloner and Networking Modules

---
+++ FileCloner/P2PFileSharing/FileSender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Networking.Communication;
+
+namespace SoftwareEngineeringGroupProject.FileCloner.P2PFileSharing;
+
+public class FileSender
+{
+    private CommunicatorServer _fileServer;
+    private const int FileServerPort = (int)SenderReceiverConstants.FileReceiverPortNumber;
+    public FileSender()
+    {
+        // create a file server in each device to serve the files
+        _fileServer = new CommunicatorServer();
+        Thread fileServerThread = new Thread(() => _fileServer.Start());
+        fileServerThread.Start();
+    }
+
+}
