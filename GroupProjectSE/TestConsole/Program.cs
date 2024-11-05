285a5bcd79532a6fe8f7cf87bffd78500335e6cb
NEERAJ KRISHNA N <neerajnjps03@gmail.com>
2024-11-05 23:05:02 +0530

    Test Console app added

---
+++ GroupProjectSE/TestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using GroupProjectSE.FileCloner;
+using GroupProjectSE.FileCloning.FileSharing;
+
+namespace TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileCloner cloner = new();
+            FileReceiver fileReceiver = cloner._fileReceiver;
+            FileSender fileSender = cloner._fileSender;
+
+            fileReceiver.RequestFiles();
+
+        }
+    }
+}
